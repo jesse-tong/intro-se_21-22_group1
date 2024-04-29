c4230fad098269a5e718b298bae2dc67124267cf
duccuong1609 <duccuong1609@gmail.com>
2024-04-29 12:29:31 +0700

    Add comment session part

--- src/components/page_components/comment_components/CommentSection.vue
+++ src/components/page_components/comment_components/CommentSection.vue
@@ -1,8 +1,24 @@
 <template>
     <div>
       <div class="">
+        <div v-if="accountStore.loggedIn">
+          <div class="input-group">
+              <label for="addCommentBookId" class="input-group-text"><span>Book ID: </span></label>
+              <input id="addCommentBookId" :value="$props.bookId" disabled class="form-control" />
+          </div>
+          <div class="input-group my-2">
+              <label for="addCommentRating" class="input-group-text" ><span>Rating:</span></label>
+              <input class="form-control" type="number" min="0" max="10" v-model.number="commentRating" id="addCommentRating"/>
+          </div>
+          <div class="form-floating">
+              <textarea class="form-control" v-model="commentContent"></textarea>
+              <label for="commentContent"></label>
+          </div>
+          <button class="btn btn-primary mt-2" @click="addComment"><span>Add comment</span></button>
+          <hr class="hr" />
+        </div>
         <div class="col">
-          
+          <CommentCard v-for="comment in comments" :comment="comment" @updateCommentList="()=>fetchComments(currentPage)" />
           
           <ul class="pagination">
                 <li class="page-item">
@@ -22,48 +38,134 @@
   
   <script>
   import { useAccountStore } from '../../stores/LoginInfoStore';
-  
+  import {  mapStores } from 'pinia';
+  import CommentCard from './CommentCard.vue';
+  import axios from 'axios';
   export default {
     props: {
-      comments: Array,
+      bookId: {
+        type: Number,
+        required: true
+      },
       perPage: {
         type: Number,
-        default: 5,
+        default: 10,
       },
     },
     data() {
       return {
         currentPage: 1,
-        selectedCommentId: null,
-
+        comments: [],
+        commentRating: null,
+        commentContent: '',
       }
     },
     computed: {
-      store() {
-        return useAccountStore();
-      },
-      paginatedComments() {
-        const start = (this.currentPage - 1) * this.perPage
-        return this.comments.slice(start, start + this.perPage)
-      },
+      ...mapStores(useAccountStore)
     },
     watch: {
-      comments: {
-        immediate: true,
-        handler(val) {
-          this.totalPages = Math.ceil(val.length / this.perPage)
+      
+      currentPage: {
+        handler(newCurrentPage, oldCurrentPage){
+          this.fetchComments(newCurrentPage);
         },
-      },
+        immediate: true
+      }
     },
     methods: {
       changePage(page) {
-        this.currentPage = Math.max(1, Math.min(this.totalPages, page))
+        this.currentPage = Math.max(1, page)
       },
-      onConfirmEditComment(commentId){
+      fetchComments(page){
+        axios.get('/api/comment/' + this.$props.bookId, {
+            params: {
+              page: page,
+              limit: this.$props.perPage
+            }
+          }).then(response=> {
+                if (!response.data || response.data == undefined || !response.data.success){
+                    this.$notify({
+                        title: "Fetch comments failed",
+                        text: "Fetch comments failed with unknown error, this can be from network or server",
+                        type: "error"
+                    });
 
-        this.selectedCommentId = null;
+                    return;
+                }
+                if (response.data.success === true){
+                    
+                    this.comments = response.data.result;
+                }else{
+                    this.$notify({
+                        title: "Fetch comments failed",
+                        text: "Fetch comments failed with error: " + response.data.error,
+                        type: "error"
+                    });
+                    
+                }   
+            }).catch(err=>{
+                this.$notify({
+                    title: "Fetch comments failed",
+                    text: "Fetch comments failed with error: " + err.response.data.error,
+                    type: "error"
+                })
+            }).finally(()=>{
+                
+            });
       },
+      addComment(){
+        if (this.commentRating === null || this.commentRating < 0 || this.commentRating > 10){
+          this.$notify({
+            title: "No rating or invalid comment rating!",
+            text: "No rating or invalid comment rating",
+            type: error
+          });
+          return;
+        }
+        axios.postForm("/api/comment", {
+          book_id: this.$props.bookId,
+          comment: this.commentContent,
+          rating: this.commentRating
+        }).then(response=> {
+                if (!response.data || response.data == undefined || !response.data.success){
+                    this.$notify({
+                        title: "Add comment failed",
+                        text: "Fetch comments failed with unknown error, this can be from network or server",
+                        type: "error"
+                    });
+
+                    return;
+                }
+                if (response.data.success === true){
+                    
+                  this.$notify({
+                    title: "Add comment successfully!",
+                    text: "Add comment successfully with comment ID: "+ response.data.result.id,
+                    type: "success"
+                  });
+                  this.fetchComments(this.currentPage);
+                }else{
+                    this.$notify({
+                        title: "Fetch comments failed",
+                        text: "Fetch comments failed with error: " + response.data.error,
+                        type: "error"
+                    });
+                    
+                }   
+            }).catch(err=>{
+                this.$notify({
+                    title: "Fetch comments failed",
+                    text: "Fetch comments failed with error: " + err.response.data.error,
+                    type: "error"
+                })
+            }).finally(()=>{
+                
+            });
+      }
     },
+    components: {
+      CommentCard: CommentCard
+    }
   }
   </script>
   
