801856ad7bc3fa0f5c3ca7ae1089fb6ab7d3cec3
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2024-06-17 15:26:17 +0700

    Move user's current borrow statuses to user settings

--- src/router.js
+++ src/router.js
@@ -6,14 +6,14 @@ import BorrowManagement from './components/page_components/borrow_management_com
 import PageNotFound from './components/views/PageNotFound.vue';
 import LoginPage from './components/views/LoginPage.vue';
 import BookManagement from './components/page_components/book_management_components/BookManagement.vue';
-import BorrowTableUser from './components/page_components/user_borrow_components/BorrowTableUser.vue';
+//import BorrowTableUser from './components/page_components/user_borrow_components/BorrowTableUser.vue';
 import BookByAuthors from './components/page_components/book_page_components/BookByAuthors.vue';
 import BookByGenres from './components/page_components/book_page_components/BookByGenres.vue';
 import NavBar from './components/page_components/NavBar.vue';
 import RegisterPage from './components/views/RegisterPage.vue';
 import AdvancedSearch from './components/page_components/advanced_search_components/AdvancedSearch.vue';
-import UserProfile from './components/views/UserProfile.vue';
-import UserSettings from './components/views/UserSettings.vue';
+import UserProfile from './components/page_components/user_settings_components/UserProfile.vue';
+import UserSettings from './components/page_components/user_settings_components/UserSettings.vue';
 import LibraryPolicyPage from './components/views/LibraryPolicyPage.vue';
 import Footer from './components/views/Footer.vue';
 import HomePage from './components/views/HomePage.vue';
@@ -87,12 +87,6 @@ const routes = [
         },
         props: {
           default: true
-        }
-      },
-      {
-        path: 'user/borrows',
-        components: {
-          default: BorrowTableUser
         }
       },
       {
