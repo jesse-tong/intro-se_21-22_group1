72304d4da4224f4e75a0ec09f990d52469fad41a
VoQuocBao123 <voquocbao123@gmail.com>
2024-06-18 18:22:08 +0700

    Add verification to policies settings APIs and add navigation guards for admin-only paths

--- src/main.js
+++ src/main.js
@@ -8,11 +8,12 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import 'font-awesome/css/font-awesome.min.css';
 import axios from 'axios';
 import { createPinia } from 'pinia'
-import Notifications from '@kyvg/vue3-notification';
+import Notifications, { useNotification } from '@kyvg/vue3-notification';
 import { router } from './router';
 
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js'
-  
+import { useAccountStore } from './components/stores/LoginInfoStore';
+
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement);
 
 axios.defaults.baseURL = 'http://localhost:5000';
@@ -25,7 +26,28 @@ app.config.globalProperties = {
     apiSite: 'http://localhost:5000'
 }
 app.use(Notifications)
-app.use(router)
 app.use(pinia)
+
+const accountStore = useAccountStore();
+const notify = useNotification();
+
+router.beforeEach(async (to, from) => {
+    if (
+      // make sure the user is authenticated
+      !accountStore.isAdmin &&
+      //Check if the page is an admin-only one, avoid an infinite redirect
+      to.fullPath.includes('/admin/') && to.name !== 'Login'
+    ) {
+      // notifies user that the page is admin-only redirect the user to the login page
+      notify.notify({
+        title: 'Admin-only page!',
+        text: 'Page with path ' + to.fullPath + ' is only for admin, please login to an admin account!',
+        type: 'error'
+      });
+      return { name: 'Login' }
+    }
+})
+
+app.use(router)
 
 app.mount('#app')
