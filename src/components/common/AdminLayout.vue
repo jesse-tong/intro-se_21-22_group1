dbb5cb47fed1a9cdd4143ef14416b9ff0425712f
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2024-09-13 14:32:46 +0700

    Fix UI

--- src/components/common/AdminLayout.vue
+++ src/components/common/AdminLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
-        <AdminSidebar class="d-none d-lg-block col-lg-3 col-xl-2 col-xxl-2" />
-        <div class="col-12 col-lg-9 col-xl-10 col-xxl-10">
+        <AdminSidebar class="d-none d-lg-block col-lg-2 col-xl-2 col-xxl-2" />
+        <div class="col-12 col-lg-10 col-xl-10 col-xxl-10">
             <RouterView></RouterView>
         </div>
     </div>
